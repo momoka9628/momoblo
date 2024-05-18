d1d96f05d46ef67cbd13d7958e872f5c930ca6a4
wakita <wakita@sigmaint.co.jp>
2024-05-18 14:23:23 +0900

    a

--- components/SideMenu/index.tsx
+++ components/SideMenu/index.tsx
@@ -1,25 +1,28 @@
-import styles from './index.module.css'
+import styles from './index.module.css';
 import { getTagList } from '@/libs/microcms';
 import { LIMIT } from '@/constants';
 import TagList from '@/components/TagList';
 import MonthList from '@/components/MonthList';
 import { Tag } from '@/libs/microcms';
 import SearchField from '@/components/SearchField';
+import { Suspense } from 'react';
 
 type Props = {
   tags?: Tag[];
 };
 
 export default async function SideMenu() {
-  const tags = await getTagList({ limit: LIMIT })
+  const tags = await getTagList({ limit: LIMIT });
   return (
     <div className={styles.side}>
-      <SearchField />
+      <Suspense>
+        <SearchField />
+      </Suspense>
       <h3 className={styles.title}>タグ一覧</h3>
       <TagList tags={tags.contents} />
       <h3 className={styles.title}>月別アーカイブ</h3>
       {/* @ts-expect-error Server Component */}
       <MonthList />
     </div>
-  )
+  );
 }
